401c04e9387e1c68b1139e6046540b2d6544a029
scoder5 <scoding83@gmail.com>
2023-03-12 20:14:09 +0530

    Fetching data from the admin

--- tubers/webpages/models.py
+++ tubers/webpages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Slider(models.Model):
+    headline = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    button_text = models.CharField(max_length=255)
+    button_link = models.CharField(max_length=255, default='DEFAULT VALUE')
+    photo = models.ImageField(upload_to='media/slider/%Y/')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.headline
