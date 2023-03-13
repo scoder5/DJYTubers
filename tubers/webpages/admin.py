3bd5364a82f507c0dd18748f023dbf1866389edd
scoder5 <scoding83@gmail.com>
2023-03-13 20:39:54 +0530

    custom admin

--- tubers/webpages/admin.py
+++ tubers/webpages/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Slider, Team
 
 # Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'role', 'created_date')
+    list_display_links = ('id', 'first_name')
+    search_fields = ('first_name', 'role')
+    list_filter = ('role',)
+
 admin.site.register(Slider)
-admin.site.register(Team)
+admin.site.register(Team, TeamAdmin)
