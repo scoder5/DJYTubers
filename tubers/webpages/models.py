d9907ee25835049f6d2257565bf951c869fbd83e
scoder5 <scoding83@gmail.com>
2023-03-15 18:41:55 +0530

    custom admin panel

--- tubers/webpages/models.py
+++ tubers/webpages/models.py
@@ -19,6 +19,7 @@ class Team(models.Model):
     role = models.CharField(max_length=255)
     fb_link = models.CharField(max_length=255)
     insta_link = models.CharField(max_length=255)
+    youtube_link = models.CharField(max_length=255, default='DEFAULT VALUE')
     photo = models.ImageField(upload_to='media/team/%Y/%m/%d/')
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
