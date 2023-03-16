8685ba8372dbb2cf5cd36f3b086054b4d13736f4
scoder5 <scoding83@gmail.com>
2023-03-16 19:39:56 +0530

    new commit

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Youtuber
 
 # Create your views here.
 def youtubers(request):
-    pass
+    tubers = Youtuber.objects.order_by('-created_date')
+
+    data = {
+        'tubers': tubers,
+    }
+    
+    return render(request, 'youtubers/youtubers.html', data)
 
 def youtubers_detail(request, id):
     pass
