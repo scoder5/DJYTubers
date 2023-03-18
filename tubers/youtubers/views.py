79d489bcbd23d6ee7679e216f6bcd92247495381
scoder5 <scoding83@gmail.com>
2023-03-18 18:12:58 +0530

    search bar updated

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -19,14 +19,40 @@ def youtubers_detail(request, id):
 def search(request):
     #POST request is a request that gets stored into the database
     #GET request is not stored into the database
+
     tubers = Youtuber.objects.order_by('-created_date')
+    # city_search = Youtuber.objects.all()
+    # camera_search = Youtuber.objects.all()
+    # category_search = Youtuber.objects.all()
+    city_search = Youtuber.objects.values_list('city', flat=True).distinct()
+    camera_search = Youtuber.objects.values_list('camera_type', flat=True).distinct()
+    category_search = Youtuber.objects.values_list('category', flat=True).distinct()
 
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
             tubers = tubers.filter(name__icontains=keyword)
 
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            tubers = tubers.filter(city__iexact=city)
+    
+    if 'camera_type' in request.GET:
+        camera_type = request.GET['camera_type']
+        if camera_type:
+            tubers = tubers.filter(camera_type__iexact=camera_type)
+
+    if 'category' in request.GET:
+        category = request.GET['category']
+        if category:
+            tubers = tubers.filter(category__iexact=category)
+
     data = {
         'tubers': tubers,
+        'city_search': city_search,
+        'camera_search': camera_search,
+        'category_search': category_search,
     }
+
     return render(request, 'youtubers/search.html', data)
